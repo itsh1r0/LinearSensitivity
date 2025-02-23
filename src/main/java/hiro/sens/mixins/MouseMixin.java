24013130fba9e0a4905de0199cb654252d888c7a
ItsukaHiro <itsukahiro.personal@proton.me>
2025-02-24 04:46:37 +0700

    Fix sens scale
    update various things

--- src/main/java/hiro/sens/mixins/MouseMixin.java
+++ src/main/java/hiro/sens/mixins/MouseMixin.java
@@ -12,13 +12,18 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(Mouse.class)
 public class MouseMixin {
 
+    //DON'T CHANGE `ordinal`
+    //for some reason:
+    //ordinal: 0 or >=6 => Game Crash instantly
+    //ordinal: 1 => Affect `i` variable
+    //ordinal: 2 => Affect `j` variable
+    //ordinal: 3 => Affect `e` variable
+    //ordinal: 4 => Affect `g` variable
+    //ordinal: 5 => Affect `h` variable
+    //yep, there's no `d`. Seem like:
+        //double e = Math.pow((Double)this.client.options.getMouseSensitivity().getValue() * (double)0.6F + (double)0.2F, 3);
     @ModifyVariable(method = "updateMouse", at = @At("STORE"), ordinal = 3)
-    private double modifyBaseSensitivity(double originalD) {
-        return MinecraftClient.getInstance().options.getMouseSensitivity().getValue();
-    }
-
-    @ModifyVariable(method = "updateMouse", at = @At("STORE"), ordinal = 4)
-    private double modifyScaleSensitivity(double originalD) {
+    private double modifyBaseSensitivity(double originalE) {
         return MinecraftClient.getInstance().options.getMouseSensitivity().getValue();
     }
 }
